c84a17bf217b1768525b7594c3093e4c85d0ac46
Fatjon Mileti <91939874+FatjonMileti@users.noreply.github.com>
2022-11-26 19:47:20 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(
+(res) => {
+console.log("Success!!.")
+}).catch((e) => {console.log("error is: " + e);});
+
 
 let Person;
 
